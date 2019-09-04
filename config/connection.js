f20a37f5ebb50de02a3feab4a91b54eff1cddecb
nargesfamili <narges.fayazian@gmail.com>
2019-09-04 11:52:51 -0400

    v1

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  user: "xezr07e1hqvwm469",
+  password: "qf8kbv0lvrliscx2",
+  database: "tfxnuofy7u5nuy95",
+  host: "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  dialect: "mysql"
+
+  // host: "localhost",
+  // user: "root",
+  // password: "Newjob18",
+  // database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting:" + err.stack);
+    return;
+  }
+  console.log("connected as id" + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cincinnati Slider', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Perfect Patty Melt', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheese', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Masterpiece', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chef Steve Special', FALSE);
+
